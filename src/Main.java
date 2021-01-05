36097a01935b4ad0a7a0e91feb93bf9384976c45
gfzum <w48905833@163.com>
2021-01-05 16:59:35 +0800

    3

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("newnewnew");
-        System.out.println("olld");
+        System.out.println("ollld");
     ////rtttt
         System.out.println();
     }
