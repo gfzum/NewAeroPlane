b9edf5903946cd21d4cd410f1ebf9bcbd953fe93
gfzum <w48905833@163.com>
2021-01-05 16:55:43 +0800

    test

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("newnewnew");
-        System.out.println("old");
+        System.out.println("olld");
+    ////rtttt
+        System.out.println();
     }
 }
